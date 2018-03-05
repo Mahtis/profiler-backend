28e5140206cdbc2466b8df21ae8d2af144f9c47d
Tiainen <mitiaine@lm7-cs-18.local>
2018-03-05 20:23:24 +0200

    creating models

--- src/models/index.js
+++ src/models/index.js
@@ -16,13 +16,61 @@ const Profile = sequelize.define('profile', {
     timestamps: true
 })
 
-const Question
+const Question = sequelize.define('question', {
+    id: {
+        primaryKey: true,
+        type: Sequelize.BIGINT
+    },
+    text: { type: Sequelize.STRING }
+},
+{
+    tableName: 'question',
+    timestamps: false
+})
 
-const ProfileQuestion
+const ProfileQuestion = sequelize.define('profile_question', {
+    id: {
+        primaryKey: true,
+        type: Sequelize.BIGINT
+    },
+    profile_id: { type: Sequelize.BIGINT },
+    question_id: { type: Sequelize.BIGINT },
+    correct_response: { type: Sequelize.BIGINT }
+},
+{
+    tableName: 'profile_question',
+    timestamps: false
+})
 
-const Response
+const Response = sequelize.define('response', {
+    id: {
+        primaryKey: true,
+        type: Sequelize.BIGINT
+    },
+    response_option_id: { type: Sequelize.BIGINT },
+    account_id: { type: Sequelize.BIGINT },
+    profile_question_id: { type: Sequelize.BIGINT }
+},
+{
+    tableName: 'response',
+    timestamps: true
+})
 
-const ResponseOption
+const ResponseOption = sequelize.define('response_option', {
+    id: {
+        primaryKey: true,
+        type: Sequelize.BIGINT
+    },
+    option_value: { type: Sequelize.STRING }
+})
 
-const Account
+const Account = sequelize.define('account', {
+    id: {
+        primaryKey: true,
+        type: Sequelize.UUIDV4
+    },
+    name: { type: Sequelize.STRING },
+    birthdate: { type: Sequelize.DATEONLY },
+    email: { type: Sequelize.STRING }
+})
 
